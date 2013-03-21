16d8bbed1e2e8ed6ca76f2681c01ccd4c0c336c0
unknown <volkan_halil@SERHATG.sekerbilisim.com.tr>
2013-03-21 15:30:42 +0200

    Volkan: Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CSProjectTrackingWithExcel::Application.config.secret_token = '7b741d2b99cd3635bbee3ef22a92f92a52401971ab751091a493104fe2387189094f795c7a5b1f5b8c92beb5b3aa53a7b038adb7964479a9021d40d83636a69e'
